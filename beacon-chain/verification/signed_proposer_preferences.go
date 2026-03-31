083f5f07c777b6d44a907a4d3bcfa94a69d2a5f9
Bharath Vedartham <vedabharath12345@gmail.com>
2026-03-31 23:38:47 +0530

    sign fix

--- beacon-chain/verification/signed_proposer_preferences.go
+++ beacon-chain/verification/signed_proposer_preferences.go
@@ -71,7 +71,11 @@ func (v *ProposerPreferencesVerifier) VerifySignature(st state.ReadOnlyBeaconSta
 
 	msg := v.message()
 	epoch := slots.ToEpoch(msg.ProposalSlot)
-	domain, err := signing.Domain(st.Fork(), epoch, params.BeaconConfig().DomainProposerPreferences, st.GenesisValidatorsRoot())
+	fork, err := params.Fork(epoch)
+	if err != nil {
+		return errors.Wrap(err, "fork")
+	}
+	domain, err := signing.Domain(fork, epoch, params.BeaconConfig().DomainProposerPreferences, st.GenesisValidatorsRoot())
 	if err != nil {
 		return errors.Wrap(err, "domain")
 	}
